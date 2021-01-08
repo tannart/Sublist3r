2932e68ecc618f35514b53e9a96d762b14dcc69b
Jonathan Baker <jonathan@tail.at>
2021-01-08 15:16:05 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Sublist3r-lib',
-    version='0.2',
+    version='0.2.1',
     python_requires='>=2.7',
     install_requires=['dnspython', 'requests', 'argparse; python_version==\'2.7\''],
     packages=find_packages(),
